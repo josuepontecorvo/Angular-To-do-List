cda4a2f97e5d29a6de5fa6239a6cf8625aaf19e4
Josue Pontecorvo <josuepontecorvo@gmail.com>
2022-08-15 17:08:27 -0300

    To do list running

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Todo } from "./todo";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'hello-world';
+
+  todoValue:string = "";
+  list:Todo[] = [];
+
+  ngOnInit () {
+    this.list = [];
+    this.todoValue = "";
+  }
+
+  addItem() {
+    if (this.todoValue !== "") {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false
+      }
+      this.list.push(newItem);
+    }
+    this.todoValue = "";
+  }
+
+  delete(id:number) {
+    this.list = this.list.filter(item => item.id !== id);
+  }
 }
---
+++ src/app/todo.ts
@@ -0,0 +1,6 @@
+// we write here the rules that must follow the interface not the functionality.
+export interface Todo{
+    id: number;
+    value: string;
+    isDone: boolean;
+}
